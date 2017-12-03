8c547dff9a7c95563f7b0bb7bc9c07bda7123dae
CVX1 <32749006+CVX1@users.noreply.github.com>
2017-12-03 12:33:48 -0800

    Fixing constructor/destructor structure

--- src/composite.cpp
+++ src/composite.cpp
@@ -1,5 +1,3 @@
-#ifndef __COMPOSITE_CPP__
-#define __COMPOSITE_CPP__
 #include "composite.h"
 
 //--------------------------------------------------------------------------
@@ -13,33 +11,31 @@ void Instruction::setString(char* s) {str = s;}
 //--------------------------------------------------------------------------
 // Command Class
 //--------------------------------------------------------------------------
-Command::Command() : Instruction(), {}
-Command::Command(char* s) : Instruction(), Instruction(s) {};
-Command::~Command() : Instruction(), {}
-        /*
-        void execute() {
-            // execute using execvp and vipid //
-            // and assign to exec             
+Command::Command() : Instruction() {}
+Command::Command(char* s) : Instruction(s) {};
+Command::~Command() : Instruction() {}
+/*
+void execute() {
+    // execute using execvp and vipid //
+    // and assign to exec             
 
-            
-        }
-        */
+
+}
+*/
 
 void Command::print() {cout << str << " ";}
 
 //--------------------------------------------------------------------------
 // Connector Class
 //--------------------------------------------------------------------------
-Connector::Connector() : Instruction(), {}
-Connector::Connector(char* s) : Instruction(), Instruction(s) {
+Connector::Connector() : Instruction() {}
+Connector::Connector(char* s) : Instruction(s) {
     if (strcmp(s, ";") == 0) {exec = true;}
     else if (strcmp(s, "&&") == 0) {exec = true;}
     else if (strcmp(s, "||") == 0) {exec = false;}
     else {exec = true;}
 }
-Connector::~Connector() : Instruction(), {
-        
-        }
+Connector::~Connector() : Instruction(){}
 
 void Connector::print() {cout << str << " ";}
 
@@ -48,23 +44,21 @@ void Connector::print() {cout << str << " ";}
 //--------------------------------------------------------------------------
 // CmdString Class
 //--------------------------------------------------------------------------
-CmdString::CmdString() : Instruction(), {}
-CmdString::~CmdString() : Instruction(), {
-            for (int i = 0; i < V.size(); i++) {
-                V[i] = NULL;
-                delete V[i];
-            }
-        }
+CmdString::CmdString() : Instruction() {}
+CmdString::~CmdString() : Instruction() {
+    for (int i = 0; i < V.size(); i++) {
+        V[i] = NULL;
+        delete V[i];
+    }
+}
 
 void CmdString::addInstruction(Instruction* i) {V.push_back(i);}
 
 //void execute() {}
 
 void CmdString::print() {
-            for (unsigned int i = 0; i < V.size(); i++) {
-                V[i]->print();
-                cout << endl;
-            }
-        }
-
-#endif //__COMPOSITE_CPP__
+    for (unsigned int i = 0; i < V.size(); i++) {
+        V[i]->print();
+        cout << endl;
+    }
+}
